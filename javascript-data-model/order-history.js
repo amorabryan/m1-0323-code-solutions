a17562b50b0c4ae29b20fe3deb13d60162bbc09b
Bryan Amora <amorabryan@yahoo.com>
2023-03-24 04:04:14 +0000

    Completed javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,53 +1,53 @@
 const orderHistory = [
   {
-    total: '$34.00',
+    total: 34.00,
     orderNum: '114-3941689-8772232',
     items: [
       {
         title: 'JavaScript for Impatient Programmers',
         author: 'Rauschmayer, Dr. Axel',
         item: 'Book',
-        price: '$31.55'
+        price: 31.55
       }
     ]
   },
   {
-    total: '$44.53',
+    total: 44.53,
     orderNum: '113-9984268-1280257',
     items: [
       {
         title: 'The Timeless Way of Building',
         author: 'Alexander, Christopher',
         item: 'Book',
-        price: '$41.33'
+        price: 41.33
       }
     ]
   },
   {
-    total: '$17.22',
+    total: 17.22,
     orderNum: '114-2875557-9059409',
     items: [
       {
         title: 'GameCube Controller Adapter. Super Smash bros Switch GameCube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-GameCube Adapter',
         item: 'GameCube accessory',
-        price: '$15.98'
+        price: 15.98
       }
     ]
   },
   {
-    total: '$138.93',
+    total: 138.93,
     orderNum: '113-2883177-2648248',
     items: [
       {
         title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
         item: 'GameCube Accessory',
-        price: '$94.95'
+        price: 94.95
       },
       {
         title: 'The Art of Sql',
         author: 'Faroult, Stephane',
         item: 'Book',
-        price: '$33.99'
+        price: 33.99
       }
     ]
   }
