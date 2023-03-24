b5aec8e9a589cd00abe3281ef1ccf5411945d01e
Bryan Amora <amorabryan@yahoo.com>
2023-03-24 03:51:48 +0000

    Completed javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,28 +1,53 @@
 const orderHistory = [
   {
-    title: 'JavaScript for Impatient Programmers',
-    author: 'Rauschmayer, Dr. Axel',
-    item: 'Book'
+    total: '$34.00',
+    orderNum: '114-3941689-8772232',
+    items: [
+      {
+        title: 'JavaScript for Impatient Programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        item: 'Book',
+        price: '$31.55'
+      }
+    ]
   },
   {
-    title: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher',
-    item: 'Book'
+    total: '$44.53',
+    orderNum: '113-9984268-1280257',
+    items: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        item: 'Book',
+        price: '$41.33'
+      }
+    ]
   },
   {
-    title: 'GameCube Controller Adapter. Super Smash bros Switch GameCube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-GameCube Adapter',
-    item: 'GameCube accessory'
+    total: '$17.22',
+    orderNum: '114-2875557-9059409',
+    items: [
+      {
+        title: 'GameCube Controller Adapter. Super Smash bros Switch GameCube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-GameCube Adapter',
+        item: 'GameCube accessory',
+        price: '$15.98'
+      }
+    ]
   },
   {
-    item: [
+    total: '$138.93',
+    orderNum: '113-2883177-2648248',
+    items: [
       {
         title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-        item: 'GameCube Accessory'
+        item: 'GameCube Accessory',
+        price: '$94.95'
       },
       {
         title: 'The Art of Sql',
         author: 'Faroult, Stephane',
-        item: 'Book'
+        item: 'Book',
+        price: '$33.99'
       }
     ]
   }
