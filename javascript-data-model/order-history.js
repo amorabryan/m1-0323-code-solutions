b10712af829f421b26a5eff21fe469f641ecc645
Bryan Amora <amorabryan@yahoo.com>
2023-03-24 03:29:28 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,31 @@
+const orderHistory = [
+  {
+    title: 'JavaScript for Impatient Programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    item: 'Book'
+  },
+  {
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    item: 'Book'
+  },
+  {
+    title: 'GameCube Controller Adapter. Super Smash bros Switch GameCube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-GameCube Adapter',
+    item: 'GameCube accessory'
+  },
+  {
+    item: [
+      {
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        item: 'GameCube Accessory'
+      },
+      {
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        item: 'Book'
+      }
+    ]
+  }
+];
+
+console.log(orderHistory);
