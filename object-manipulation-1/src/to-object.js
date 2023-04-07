8e9f98f65ea3f9ad30f0315dc13dcaf95b56ac46
Bryan Amora <amorabryan@yahoo.com>
2023-04-07 02:53:57 +0000

    Revised object-manipulation-1

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,10 +1,5 @@
 /* exported toObject */
 function toObject(keyValuePair) {
-  let object = {};
-  let objectTwo = '';
-  const i = 0;
-  object[i] = keyValuePair[i];
-  objectTwo = keyValuePair[i + 1];
-  object = { [keyValuePair[i]]: objectTwo };
-  return object;
+  const [key, value] = keyValuePair;
+  return { [key]: value };
 }
